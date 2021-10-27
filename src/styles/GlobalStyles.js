1d571149b65dca1f4115e89f33523117e88f914c
Jason Amey <jason.amey@gmail.com>
2021-10-27 09:36:02 -0400

    added favicon

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,13 +1,11 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
-
 body {
     background : ${({ theme }) => theme.appBackgroundColor};
     color : ${({ theme }) => theme.appColor};
     transition : all .3s;
 }
-
 ul { 
     padding : 0px;
 }
@@ -15,7 +13,6 @@ a {
     color : ${({ theme }) => theme.appColor};
     text-decoration : none;
 }
-
 button { 
     color : ${({ theme }) => theme.appColor};
 }
